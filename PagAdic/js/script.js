868ac67f5f9278951b459cb5f667ba1cb2a7871f
Christian Muniz <christian_muniz1@hotmail.com>
2022-01-11 23:20:47 -0300

    Index.html

---
+++ PagAdic/js/script.js
@@ -0,0 +1,10 @@
+let email = document.getElementById("campo e-mail");
+
+function mostrarPopup() {
+    window.alert("Hello Word");
+}
+
+function enviarEmail() {
+    let emailDigitado = email.value;
+    console.log(emailDigitado)
+}
